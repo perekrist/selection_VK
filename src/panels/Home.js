926a5cdcd1f7b22274bffc017ea971554f125214
DuMoH112 <ew.dim@mail.ru>
2019-09-28 03:40:42 +0700

    Add horizontal scroll

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader, List, Cell } from '@vkontakte/vkui';
+import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader, View, Header, HorizontalScroll } from '@vkontakte/vkui';
 import Persik from './Persik';
 
+
+const itemStyle = {
+	flexShrink: 0,
+	width: 160,
+	height: 184,
+	display: 'flex',
+	flexDirection:
+		'column',
+	alignItems: 'center',
+	fontSize: 12
+};
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
@@ -16,13 +27,50 @@ const Home = ({ id, go, fetchedUser }) => (
 					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
 				</ListItem>
 			</Group>}
-		
-		<Group>
-			<List>
-				<Cell>Hello</Cell>
-				<Cell>World</Cell>
-			</List>
-		</Group>
+
+			<View activePanel="horizontal">
+				<Panel id="horizontal">
+					<Group style={{ paddingBottom: 8 }}>
+						<Header level="secondary">Последние добавления</Header>
+						<HorizontalScroll>
+							<div style={{ display: 'flex' }}>
+								<div style={{ ...itemStyle, paddingLeft: 4 }}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Элджей
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Ольга
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Сергей
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Илья
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Алексей
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Костя
+								</div>
+								<div style={itemStyle}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Миша
+								</div>
+								<div style={{ ...itemStyle, paddingRight: 4 }}>
+									<Avatar size={128} style={{ marginBottom: 16 }}></Avatar>
+									Вадим
+								</div>
+							</div>
+						</HorizontalScroll>
+					</Group>
+				</Panel>
+			</View>
 
 		<Group title="Navigation Example">
 			<Div>
