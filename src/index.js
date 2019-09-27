df17ee68ec034eadee3e3d6523545370a84b2fce
DuMoH112 <ew.dim@mail.ru>
2019-09-28 00:18:25 +0700

    qwe

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
-import connect from '@vkontakte/vk-connect';
 // import registerServiceWorker from './sw';
 
 // Init VK App
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -3,16 +3,6 @@ import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
 import Persik from './Persik';
 
-const itemStyle = {
-    flexShrink: 0,
-    width: 80,
-    height: 94,
-    display: 'flex',
-    flexDirection:
-    'column',
-    alignItems: 'center',
-    fontSize: 12
-  };
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
@@ -26,53 +16,16 @@ const Home = ({ id, go, fetchedUser }) => (
 					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
 				</ListItem>
 			</Group>}
+		
+		<Group>
+			<List>
+				<Cell>Hello</Cell>
+				<Cell>World</Cell>
+			</List>
+		</Group>
 
 		<Group title="Navigation Example">
 			<Div>
-				<View activePanel="horizontal">
-					<Panel id="horizontal">
-						<PanelHeader>HorizontalScroll</PanelHeader>
-						<Group style={{ paddingBottom: 8 }}>
-							<Header level="secondary">Недавние</Header>
-							<HorizontalScroll>
-								<div style={{ display: 'flex' }}>
-									<div style={{ ...itemStyle, paddingLeft: 4 }}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Элджей
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Ольга
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Сергей
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Илья
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Алексей
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Костя
-            </div>
-									<div style={itemStyle}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Миша
-            </div>
-									<div style={{ ...itemStyle, paddingRight: 4 }}>
-										<Avatar size={64} style={{ marginBottom: 8 }}><Persik /></Avatar>
-										Вадим
-            </div>
-								</div>
-							</HorizontalScroll>
-						</Group>
-					</Panel>
-				</View>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
 					КРИСТИНА, ПРИВЕТ
 				</Button>
