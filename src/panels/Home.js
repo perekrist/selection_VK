aa5f8a08dd7d1d610457a9d2ec51baa9c11b56a2
DuMoH112 <ew.dim@mail.ru>
2019-09-28 00:00:38 +0700

    qwe

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,21 +2,76 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
 
+const itemStyle = {
+    flexShrink: 0,
+    width: 80,
+    height: 94,
+    display: 'flex',
+    flexDirection:
+    'column',
+    alignItems: 'center',
+    fontSize: 12
+  };
+
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Example</PanelHeader>
 		{fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<ListItem
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</ListItem>
-		</Group>}
+			<Group title="User Data Fetched with VK Connect">
+				<ListItem
+					before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200} /> : null}
+					description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+				>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</ListItem>
+			</Group>}
 
 		<Group title="Navigation Example">
 			<Div>
+				<View activePanel="horizontal">
+					<Panel id="horizontal">
+						<PanelHeader>HorizontalScroll</PanelHeader>
+						<Group style={{ paddingBottom: 8 }}>
+							<Header level="secondary">Недавние</Header>
+							<HorizontalScroll>
+								<div style={{ display: 'flex' }}>
+									<div style={{ ...itemStyle, paddingLeft: 4 }}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Элджей
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Ольга
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Сергей
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Илья
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Алексей
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Костя
+            </div>
+									<div style={itemStyle}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Миша
+            </div>
+									<div style={{ ...itemStyle, paddingRight: 4 }}>
+										<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+										Вадим
+            </div>
+								</div>
+							</HorizontalScroll>
+						</Group>
+					</Panel>
+				</View>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
 					КРИСТИНА, ПРИВЕТ
 				</Button>
