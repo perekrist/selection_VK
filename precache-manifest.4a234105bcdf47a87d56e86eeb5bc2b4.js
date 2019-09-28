faa0b35dfd9f88c0976da8489209bee4f44e8554
perekrist <perekrist31@gmail.com>
2019-09-28 08:22:01 +0300

    Updates

---
+++ precache-manifest.4a234105bcdf47a87d56e86eeb5bc2b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "236a1f8c175c47f349e4",
+    "url": "./static/js/main.af7ec117.chunk.js"
+  },
+  {
+    "revision": "75cc0febade9b33166a8",
+    "url": "./static/js/2.990cdb18.chunk.js"
+  },
+  {
+    "revision": "236a1f8c175c47f349e4",
+    "url": "./static/css/main.86182b0d.chunk.css"
+  },
+  {
+    "revision": "75cc0febade9b33166a8",
+    "url": "./static/css/2.ba4716ba.chunk.css"
+  },
+  {
+    "revision": "33004422d1dddad34851b0de349599bc",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58d5db9f07324621a21a3ffea900a40e.js"
+  "./precache-manifest.4a234105bcdf47a87d56e86eeb5bc2b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
